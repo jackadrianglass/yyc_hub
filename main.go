31855b8ed6145b6e997ea927e5d2a424ac68191d
glassjack <jackadrianglass@gmail.com>
2025-03-09 20:13:41 -0600

    chore: Swap over to fiber
    Wasn't smart enough to figure out how to get the gin to parse the actual
    body from a form even with json encoding. Sooooo I switched to something
    else that has better documentation and I could actually get up and running.
    Plus, fiber is a good name and I like it.

--- main.go
+++ main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/template/html/v2"
 )
 
+type TestForm struct {
+	Email   string `form:"email" json:"email" binding:"required"`
+	Message string `form:"message" json:"message" binding:"required"`
+}
+
 func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{})
-	})
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "ping.tmpl", gin.H{})
-	
+	app := fiber.New(fiber.Config{
+		Views: html.New("./templates", ".tmpl"),
 	})
 
-	router.Run(":8080")
+	app.Get("/", func(ctx fiber.Ctx) error {
+		return ctx.Render("index", fiber.Map{})
+	})
+
+	app.Post("/test-form", func(ctx fiber.Ctx) error {
+		var form TestForm
+		if err := ctx.Bind().Body(&form); err != nil {
+			return ctx.Status(fiber.StatusBadRequest).SendString("Bad Request")
+		}
+
+		return ctx.Render("test-form-rsp", fiber.Map{
+			"email": form.Email,
+			"message": form.Message,
+		})
+	})
+
+	app.Listen(":3000")
 }
