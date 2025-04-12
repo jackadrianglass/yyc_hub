3f14180c00c2fe6b961959b2945acaf72e6b7f12
glassjack <jackadrianglass@gmail.com>
2025-04-12 10:28:56 -0600

    feat: Add gorm and a bunch of data models

--- go.mod
+++ go.mod
@@ -25,6 +25,8 @@ require (
 	github.com/gofiber/utils/v2 v2.0.0-beta.7 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
@@ -32,7 +34,7 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/mattn/go-sqlite3 v1.14.27 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
@@ -49,7 +51,9 @@ require (
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
+	gorm.io/gorm v1.25.12 // indirect
 )
--- main.go
+++ main.go
@@ -1,103 +1,118 @@
 package main
 
 import (
-	"database/sql"
-	_ "fmt"
+	_ "database/sql"
+	"fmt"
+	"time"
 
-	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/template/html/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	_ "github.com/gofiber/fiber/v3"
+	_ "github.com/gofiber/template/html/v2"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// DB Models
+
+type User struct {
+	gorm.Model
+	Name       string
+	Email      string `gorm:"uniqueIndex"`
+	Age        int
+	Profile    Profile
+	Posts      []Post
+}
+
+type Group struct {
+	gorm.Model
+	Name string
+	MeetupGroupName *string
+}
+
+type Profile struct {
+	gorm.Model
+	UserID uint
+	Bio    string
+}
+
+type Post struct {
+	gorm.Model
+	UserID  uint
+	Title   string
+	Content string
+}
+
+type Event struct {
+	gorm.Model
+	Title       string
+	Description string
+	Date        time.Time
+	Location    string
+}
+
+type MeetupEvent struct {
+	gorm.Model
+	// todo: Link it to events table
+	MeetupId string `gorm:"uniqueIndex"`
+	Dynamic bool
+	GroupName string
+}
+
+// front end stuff
 
 type TestForm struct {
 	Email   string `form:"email" json:"email" binding:"required"`
 	Message string `form:"message" json:"message" binding:"required"`
 }
 
-type EventA struct {
-	Title       string
-	Description string
-	// Date        string // todo: Make this into a date time object
-	// Location    string // todo: Strongly typed address?
-}
-
-type EventDb struct {
-	db *sql.DB
-}
-
-func OpenConnection() (*EventDb, error) {
-	const file string = "test.db"
-
-	db, err := sql.Open("sqlite3", file)
-	if err != nil {
-		return nil, err
-	}
-
-	const create string = `
-		CREATE TABLE IF NOT EXISTS events (
-		id INTEGER NOT NULL PRIMARY KEY,
-		title TEXT
-		description TEXT
-		);`
-
-	if _, err := db.Exec(create); err != nil {
-		return nil, err
-	}
-
-	return &EventDb{
-		db: db,
-	}, nil
-}
-
-func (self *EventDb) Insert(event EventA) (int, error) {
-	res, err := self.db.Exec("INSERT INTO activities VALUES(NULL,?,?);", event.Title, event.Description)
-	if err != nil {
-		return 0, err
-	}
-
-	var id int64
-	if id, err = res.LastInsertId(); err != nil {
-		return 0, err
-	}
-	return int(id), nil
-}
-
 func main() {
-	// db, error := OpenConnection()
-	// if error != nil {
-	// 	fmt.Println(error)
-	// 	return
-	// }
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
 
-	// id, error := db.Insert(EventA { Title: "awesome sauce", Description: "Saucy awesomeness" })
-	// if error != nil {
-	// 	fmt.Println(error)
-	// 	return
-	// }
-	// fmt.Println(id)
-	// fmt.Println("success!")
-	
+	db.AutoMigrate(&User{}, &Profile{}, &Post{})
 
-
-	app := fiber.New(fiber.Config{
-		Views: html.New("./templates", ".tmpl"),
+	db.Create(&User{
+		Name:    "John",
+		Email:   "john@example.com",
+		Age:     30,
+		Profile: Profile{Bio: "Software developer"},
 	})
 
-	app.Get("/", func(ctx fiber.Ctx) error {
-		return ctx.Render("index", fiber.Map{})
-	})
+	var user User
+	db.First(&user, 1)
+	fmt.Println(user)
+	db.First(&user, "email = ?", "john@example.com")
+	fmt.Println(user)
 
-	app.Post("/test-form", func(ctx fiber.Ctx) error {
-		var form TestForm
-		if err := ctx.Bind().Body(&form); err != nil {
-			return ctx.Status(fiber.StatusBadRequest).SendString("Bad Request")
-		}
+	db.Preload("Profile").Preload("Posts").Find(&user)
 
-		return ctx.Render("test-form-rsp", fiber.Map{
-			"email":   form.Email,
-			"message": form.Message,
-		})
-	})
+	db.Model(&user).Update("Name", "John Doe")
+	db.Model(&user).Updates(User{Name: "John Doe", Age: 31})
 
-	app.Listen(":3000")
+	db.Delete(&user)
+
+	// app := fiber.New(fiber.Config{
+	// 	Views: html.New("./templates", ".tmpl"),
+	// })
+
+	// app.Get("/", func(ctx fiber.Ctx) error {
+	// 	return ctx.Render("index", fiber.Map{})
+	// })
+
+	// app.Post("/test-form", func(ctx fiber.Ctx) error {
+	// 	var form TestForm
+	// 	if err := ctx.Bind().Body(&form); err != nil {
+	// 		return ctx.Status(fiber.StatusBadRequest).SendString("Bad Request")
+	// 	}
+
+	// 	return ctx.Render("test-form-rsp", fiber.Map{
+	// 		"email":   form.Email,
+	// 		"message": form.Message,
+	// 	})
+	// })
+
+	// app.Listen(":3000")
 }
